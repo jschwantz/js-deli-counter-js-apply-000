2f14a6e6e0be4925f914a2e9ac8ee3b4c5fe3758
Jared Schwantz <c.jared.schwantz@gmail.com>
2019-05-22 01:59:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function takeANumber(curline, person) {
-  return 'Welcome, ' + person + '. You are number '+(curline.length + 1) + ' in line.'
+  
+  curline.push(person);
+  
+  return 'Welcome, ' + person + '. You are number '+curline.length + ' in line.';
 }
